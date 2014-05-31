9c57039a0368c3864a4faed96d13ffafc02a03aa
rmacaspac <rmacaspac@gmail.com>
2014-05-30 17:01:10 -0700

    Initial Commit

---
+++ Lifted/RMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RMAppDelegate.h
+//  Lifted
+//
+//  Created by Ryan Macaspac on 5/30/14.
+//  Copyright (c) 2014 Ryan Macaspac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lifted/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lifted
+//
+//  Created by Ryan Macaspac on 5/30/14.
+//  Copyright (c) 2014 Ryan Macaspac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RMAppDelegate class]));
+    }
+}
